647fd7344d4095139f45ef3149fc5c2cc7b1d3f8
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2020-01-22 16:55:24 +0100

    PHPSnack 2 completato

--- 2#php-snacks-blocco-1/index.php
+++ 2#php-snacks-blocco-1/index.php
@@ -7,4 +7,23 @@
 // ● “age” sia un numero.
 // Se tutto è ok, stampare “Accesso riuscito”, altrimenti “Accesso negato”
 
+$name = $_GET['name'];
+$mail = $_GET['email'];
+$age = $_GET['age'];
+// calcola lunghezza str e restituisce un intero
+$len_name = strlen($name);
+// cerca punto e chicciola all'interno della stringa
+$dot_mail = strpos($mail, '.');
+$at_mail = strpos($mail, '@');
+// si accerta che sia un numero
+$isAn = is_numeric($age);
+
+// se vanno tutte quante contemporaneamente (&&)
+if ($len_name > 3 && $dot_mail && $at_mail && $isAn) {
+    echo "Accesso Riuscito";
+} else {
+    // altrimenti accesso negato
+    echo "Accesso Negato";
+};
+
  ?>
